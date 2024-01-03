710a01876dce9962b22e7178b5c26dedd108e5ea
korneliushen <korneliussen06@gmail.com>
2024-01-03 12:56:03 +0100

    fjernet placeholder filtreringen for å optimalisere for mobilbrukere

--- handlelapp-kode/src/components/filter-bar.jsx
+++ handlelapp-kode/src/components/filter-bar.jsx
@@ -9,14 +9,11 @@ export default function FilterBar({search, getProductsSearch, setQ }) {
             <main className=" bg-[#24516A] w-11/12 h-16 rounded-full flex flex-col items-center justify-center">
                 <div className="w-[96%] h-12 flex flex-row items-center">
                     <section className="">
-                        <p className="text-xl">Filter:</p>
+                        <p className="text-xl"></p>
                     </section>
                     <div className="air w-8 h-full"></div>
                     <section className="flex justify-start w-full h-5/6 items-center">
                         <div className="flex w-3/4">
-                            <FilterDropdown text={"Pris"}/>
-                            <FilterDropdown text={"Kategori"}/>
-                            <FilterDropdown text={"Butikker"}/>
                         </div>
                     </section>
                     <form onSubmit={(e) => getProductsSearch(e)}>
