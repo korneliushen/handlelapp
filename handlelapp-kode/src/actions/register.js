b477fdc447db68da3da3d6133e58d9580c96ab30
korneliushen <korneliussen06@gmail.com>
2024-01-19 23:11:01 +0100

    Installert prisma og koblet til db

--- handlelapp-kode/src/actions/register.js
+++ handlelapp-kode/src/actions/register.js
@@ -1,13 +1,33 @@
 'use server'
 
 import * as z from "zod"
+import bcrypt from "bcrypt"
 
 import { RegisterSchema } from "@/lib/schemas"
+import { db } from "@/lib/db"
 
 export async function register(values) {
     const validatedFields = RegisterSchema.safeParse(values)
     if (!validatedFields.success) {
         return {error: "invalid fields"}
     }
+    const { email, password, name } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const existingUser = await db.user.findUnique({
+        where: {
+            email,
+        }
+    })
+    if (existingUser) {
+        return {error: "Email already in use"}
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    })
     return {success: "email sent"}
 }
